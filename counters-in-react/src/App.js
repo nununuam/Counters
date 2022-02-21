8ce4c8ee66181cd00ba42aadcee2c70e519315d1
nununuam <nunusnuam@gmail.com>
2022-02-21 14:55:07 -0800

    added the code for app.js from my private app.js

--- counters-in-react/src/App.js
+++ counters-in-react/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import Header from "./Header";
+import CounterList from "./CounterList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+	    super(props);
+
+	    this.state = {
+	      counters: 2
+	    };
+	  }
+  increaseCounter = () =>{
+    let addCounter = this.state.counters +1;
+    this.setState({counters: addCounter});
+  }
+  decreaseCounter = () =>{
+    let minusCounter = this.state.counters -1;
+    this.setState({ counters: minusCounter});
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header addCounter ={this.increaseCounter} minusCounter={this.decreaseCounter}/>
+	      <div>
+           <CounterList counter={this.state.counters}/>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
