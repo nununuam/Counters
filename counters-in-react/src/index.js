90b82ef47ced74c57c45e632c879a9678348ca38
nununuam <nunusnuam@gmail.com>
2022-02-21 14:58:30 -0800

    edited the indexjs

--- counters-in-react/src/index.js
+++ counters-in-react/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const data = {counters: 3};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={data}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
